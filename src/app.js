50d2295b34890390a46a0b9e473da0a47078ce23
Sabah Rahal <sabahrahal1998@gmail.com>
2022-09-01 00:51:56 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let excuse = document.getElementById("excuse");
+
+  const numRandom = arr => Math.floor(Math.random() * arr.length);
+  const randomText = () =>
+    `${who[numRandom(who)]} ${what[numRandom(what)]} ${when[numRandom(when)]}`;
+  excuse.innerHTML = randomText();
 };
